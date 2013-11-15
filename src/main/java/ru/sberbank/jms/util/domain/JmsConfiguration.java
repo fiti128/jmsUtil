a4801a39ab06be1bb430628ddd44b4dc1fddecf8
unknown <SBT-Yanushevsky-SA@SBTMINSKWSS0130.sbtm.int>
2013-11-15 17:59:47 +0300

    init commit

---
+++ src/main/java/ru/sberbank/jms/util/domain/JmsConfiguration.java
@@ -0,0 +1,18 @@
+package ru.sberbank.jms.util.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class JmsConfiguration {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3)
+    private String url;
+}
---
+++ src/test/java/ru/sberbank/jms/util/domain/JmsConfigurationDataOnDemand.java
@@ -0,0 +1,6 @@
+package ru.sberbank.jms.util.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = JmsConfiguration.class)
+public class JmsConfigurationDataOnDemand {
+}
